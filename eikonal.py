95affa0cf6922560e87f8bff9949e87714fa0f94
Your Name <hugo.belzil@gmail.com>
2025-04-05 17:31:19 -0400

    first commit

---
+++ eikonal.py
@@ -0,0 +1,138 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class ComputationalDomain:
+    def __init__(self, N, a, b, c, d):
+        self.grid =  np.round(100*np.ones((N, N)),10) #np.round(np.sqrt(2)*np.max([b-a,d-c])*np.ones((N,N)),4)
+        self.N = int(N)
+        self.h = (b-a)/(N)
+
+    def Gamma(self,stencils):
+        if type(stencils) != list:
+            raise Exception("List of coordinates for Gamma points (side-conditions) must be of type List")
+
+        for point in stencils:
+            self.grid[point]= int(0)
+
+
+test = ComputationalDomain(N=7, a=0, b=7, c=0, d=7)
+test.Gamma([(1, 1), (5, 4)])
+print(test.grid)
+print(test.h)
+
+
+def ReLU(x):
+    return np.max([x, 0])
+
+class EikonalSolver:
+    def __init__(self,domain):
+        self.domain = domain
+        self.grids_after_sweeps = []
+        self.grids_after_sweeps.append(self.domain.grid)
+
+    def update_point(self, current_grid, new_grid, i, j):
+        N = self.domain.N
+        u_old = current_grid[i, j]
+
+        if u_old == 0:
+            return # no update needed for points in Gamma
+
+        u_xmin = np.min([new_grid[i, j - 1] if j > 0 else np.inf,
+                         new_grid[i, j + 1] if j < N - 1 else np.inf])
+        u_ymin = np.min([new_grid[i - 1, j] if i > 0 else np.inf,
+                         new_grid[i + 1, j] if i < N - 1 else np.inf])
+
+        # Conditions on updating u_old
+
+        if ReLU(u_old - u_xmin) == 0 and ReLU(u_old - u_ymin) != 0:
+            u_bar = self.domain.h + u_ymin
+
+        elif ReLU(u_old - u_ymin) == 0 and ReLU(u_old - u_xmin) != 0:
+            u_bar = self.domain.h + u_xmin
+
+        elif ReLU(u_old - u_xmin) == 0 and ReLU(u_old - u_ymin) == 0:
+            return  # No update needed
+
+        else:
+            a = u_xmin
+            b = u_ymin
+
+            if np.abs(b - a) >= self.domain.h:
+                u_bar = np.min([a, b]) + self.domain.h
+
+            else:
+                u_bar = (a + b + np.sqrt(2 * self.domain.h ** 2 - (a - b) ** 2)) / 2
+
+        # UPDATE OF THE POINT
+        new_grid[i, j] = np.min([u_old, u_bar])
+
+    def Sweep1(self):
+        """Sweep going from bottom left corner of the domain to top-right corner"""
+        N = self.domain.N
+        current_grid = self.grids_after_sweeps[-1].copy()
+        new_grid = self.grids_after_sweeps[-1].copy()
+        for i in reversed(range(N)):
+            for j in range(N):
+                self.update_point(current_grid, new_grid, i, j)
+        self.grids_after_sweeps.append(new_grid) # The grid after the 1st Sweep
+
+    def Sweep2(self):
+        """Sweep going from bottom right corner of the domain to the top-left corner"""
+        N = self.domain.N
+        current_grid = self.grids_after_sweeps[-1].copy()
+        new_grid = self.grids_after_sweeps[-1].copy()
+
+        for i in reversed(range(N)):
+            for j in reversed(range(N)):
+                self.update_point(current_grid, new_grid, i, j)
+
+        self.grids_after_sweeps.append(new_grid)
+
+    def Sweep3(self):
+        """Sweep going from bottom right corner of the domain to the top-left corner"""
+        N = self.domain.N
+        current_grid = self.grids_after_sweeps[-1].copy()
+        new_grid = self.grids_after_sweeps[-1].copy()
+        for i in range(N):
+            for j in reversed(range(N)):
+                self.update_point(current_grid, new_grid, i, j)
+
+        self.grids_after_sweeps.append(new_grid)
+
+    def Sweep4(self):
+        """Sweep going from bottom right corner of the domain to the top-left corner"""
+        N = self.domain.N
+        current_grid = self.grids_after_sweeps[-1].copy()
+        new_grid = self.grids_after_sweeps[-1].copy()
+        for i in range(N):
+            for j in range(N):
+                self.update_point(current_grid, new_grid, i, j)
+        self.grids_after_sweeps.append(new_grid)
+
+    def BatchSweeps(self, k=1):
+        for i in range(k):
+            self.Sweep1()
+            self.Sweep2()
+            self.Sweep3()
+            self.Sweep4()
+
+solver = EikonalSolver(test)
+solver.BatchSweeps(k=2)
+print("AFTER SWEEP 1 : \n", solver.grids_after_sweeps[1])
+print("AFTER SWEEP 2 : \n", solver.grids_after_sweeps[2])
+print("AFTER SWEEP 3 : \n", solver.grids_after_sweeps[7])
+print("AFTER SWEEP 4 : \n", solver.grids_after_sweeps[8])
+print(len(solver.grids_after_sweeps))
+#plt.imshow(solver.grids_after_sweeps[4])
+#plt.show()
+
+
+
+
+
+
+
+
+
+
