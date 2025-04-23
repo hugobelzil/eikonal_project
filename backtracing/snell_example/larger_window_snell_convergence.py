f6196d0d1c625e7f702f8d3551648061bca8f8aa
Your Name <hugo.belzil@gmail.com>
2025-04-23 14:24:15 -0400

    plots convergence

---
+++ backtracing/snell_example/larger_window_snell_convergence.py
@@ -0,0 +1,57 @@
+import numpy as np
+from scipy.optimize import minimize_scalar
+from scipy.spatial.distance import directed_hausdorff
+from computational_domain import ComputationalDomain
+from solvers.ode_solver import ODE_backtracer
+import matplotlib.pyplot as plt
+
+#SOLVING THE EXACT PATH
+
+x0 = 0   #coordinates of starting point and target
+y0 = -0.5
+xT = 0.5
+yT = 0.5
+
+F1 = 1 #speed in the media (refraction index)
+F2 = 2
+
+def travel_time(x):
+    a = np.sqrt((x-x0)**2 + y0**2)/F1
+    b = np.sqrt((x-xT)**2 + yT**2)/F2
+    return a+b
+
+opt = minimize_scalar(travel_time, bounds = (-1,1), method = 'bounded')
+x_opt = opt.x
+
+segment1 = np.linspace([x0, y0],[x_opt,0], 50000)
+segment2 =  np.linspace([x_opt, 0], [xT, yT], 50000)
+true_path = np.vstack([segment1, segment2])
+
+#LOADING THE GRID & REPLICATING THE VARIABLES IN exampleSnell.py
+dom = ComputationalDomain(N = 801, a = -1, b = 1, c = -1, d = 1)
+dom.Gamma([(200,600)])
+grid = np.load('snell_grid.npy')
+print('h : ',dom.h)
+# COMPUTING THE PATH
+path = ODE_backtracer(x0 = [x0,y0], dt = dom.h, domain = dom, u_grid = grid)
+
+
+dt = np.linspace(3 * dom.h, dom.h, 35)
+h_distances = []
+
+for dt_value in dt:
+    print('path planning with dt=',dt_value)
+    path = ODE_backtracer(x0 = [x0,y0], dt = dt_value, domain = dom, u_grid = grid)
+    d1 = directed_hausdorff(path, true_path)[0]
+    d2 = directed_hausdorff(true_path, path)[0]
+    hausdorff_distance = max(d1, d2)
+    h_distances.append(hausdorff_distance)
+    print(hausdorff_distance)
+
+
+h_distances = np.array(h_distances)
+np.save('dt_values_larger_window.npy',np.array(dt))
+np.save('haussdorf_distances_larger_window.npy',h_distances)
+
+
+plt.show()
--- backtracing/snell_example/snell_plot.py
+++ backtracing/snell_example/snell_plot.py
@@ -26,3 +26,30 @@ plt.legend()
 plt.tight_layout()
 plt.savefig('../../plots/loglog_plot_convergence_path.png')
 plt.show()
+
+
+# SECOND PLOT SHOWING LARGER WINDOW FOR VALUES OD DELTA_T
+
+# Load the saved values
+dt_values_larger_window = np.load("dt_values_larger_window.npy")
+h_distances_larger_window = np.load("haussdorf_distances_larger_window.npy")
+
+# Log-log regression (base e)
+log_dt_larger_window = np.log(dt_values_larger_window)
+log_error_larger_window = np.log(h_distances_larger_window)
+slope, intercept = np.polyfit(log_dt_larger_window[0:15], log_error_larger_window[0:15], 1)
+
+print(f"Estimated convergence rate (slope): {slope:.3f}")
+
+# Optional: plot log-log line
+plt.figure(figsize=(8,6))
+plt.plot(log_dt_larger_window, log_error_larger_window, 'x', label='log-log points')
+plt.plot(log_dt_larger_window, slope * log_dt_larger_window + intercept, '-', label=f'Fit line (slope={slope:.3f})')
+plt.xlabel("$\log(\Delta_t)$", fontsize=16)
+plt.ylabel("$\log(d_H(S_{\Delta_t},S))$",fontsize=16)
+plt.title("Log-Log Convergence Plot of Haussdorff Distance ($d_H(S_{\Delta_t},S)$)", fontsize=16, pad = 15)
+plt.grid(True, which="both", ls="--")
+plt.legend()
+plt.tight_layout()
+plt.savefig('../../plots/loglog_plot_convergence_path_larger_window.png')
+plt.show()
